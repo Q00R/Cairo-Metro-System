7f4b83c91737ded66c0861be441b67a09f7a642f
Mostafa Ramadan <116387670+mostafaHossamEldin@users.noreply.github.com>
2023-05-23 20:24:16 +0300

    Last commit

--- routes/private/api.js
+++ routes/private/api.js
@@ -15,29 +15,30 @@ const getUser = async function (req) {
     .where("token", sessionToken)
     .innerJoin(
       "se_project.users",
-      "se_project.sessions.userid",
+      "se_project.sessions.userId",
       "se_project.users.id"
     )
     .innerJoin(
       "se_project.roles",
-      "se_project.users.roleid",
+      "se_project.users.roleId",
       "se_project.roles.id"
     )
    .first();
 
   console.log("user =>", user);
-  user.isNormal = user.roleid === roles.user;
-  user.isAdmin = user.roleid === roles.admin;
-  user.isSenior = user.roleid === roles.senior;
-  console.log("user =>", user)
+  user.isNormal = user.roleId === roles.user;
+  user.isAdmin = user.roleId === roles.admin;
+  user.isSenior = user.roleId === roles.senior;
   return user;
 };
 
 module.exports = function (app) {
   // example
-  app.get("/users", async function (req, res) {
+  app.put("/users", async function (req, res) {
     try {
        const user = await getUser(req);
+     // const {userId}=req.body
+      console.log("hiiiiiiiiiii");
       const users = await db.select('*').from("se_project.users")
         
       return res.status(200).json(users);
@@ -45,75 +46,153 @@ module.exports = function (app) {
       console.log(e.message);
       return res.status(400).send("Could not get users");
     }
-   
   });
- app.put("/api/v1/station/:stationId", async function (req, res) {
-   try {
-     const user = await getUser(req);
-     if (user.isAdmin) {
-       const { stationId } = req.params;
-       const { stationName } = req.body;
-       if (!stationId) {
-         return res.status(400).send("stationId is required");
-       }
-       const station = await db("se_project.stations")
-         .where("id", stationId)
-         .update({ stationName })
-         .returning("*");
-       return res.status(200).json(station);
-     } else {
-       return res.status(400).send("You are Unauthrized to do this action");
-     }
-   } catch (e) {
-     console.log(e.message);
-     return res.status(400).send("Could not update station");
-   }
- });
-
- app.post("/api/v1/station", async function (req, res) {
-   try {
-     const user = await getUser(req);
-     if (user.isAdmin) {
-       console.log(req.body);
-       const { stationName } = req.body;
-       if (!stationName) {
-         return res.status(400).send("name is required");
-       }
-       const station = await db("se_project.stations")
-         .insert({
-           stationName,
-           stationType: "normal",
-           stationStatus: "new created",
-         })
-         .returning("*");
-       return res.status(200).json(station);
-     } else {
-       return res.status(400).send("You are Unauthrized to do this action");
-     }
-   } catch (e) {
-     console.log(e.message);
-     return res.status(400).send("Could not create station");
-   }
- });
-
- app.delete("/api/v1/station/:stationId", async function (req, res) {
-   try {
-     const user = await getUser(req);
-     const { stationId } = req.params;
-     if (!stationId) {
-       return res.status(400).send("stationId is required");
-     }
-     const station = await db("se_project.stations")
-       .where("id", stationId)
-       .del()
-       .returning("*");
-     return res.status(200).json(station);
-   } catch (e) {
-     console.log(e.message);
-     return res.status(400).send("Could not delete station");
-   }
- });
-
-
   
+  app.post("/api/v1/station", async function (req, res) {
+    try {
+      const user = await getUser(req);
+      if (user.isAdmin){
+        const { stationName } = req.body;
+        if (!stationName) {
+          return res.status(400).send("name is required");
+        }
+        const station = await db("se_project.stations")
+          .insert({ stationName , stationType: "normal", stationStatus: "new created" })
+          .returning("*");
+        return res.status(200).json(station);
+      }
+      else
+        return res.status(400).send("You are Unauthorized to do this action")
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not create station");
+    }
+  });
+
+  app.put("/api/v1/station/:stationId", async function (req, res) {
+    try {
+      const user = await getUser(req);
+      if(user.isAdmin){
+        const { stationId } = req.params;
+        const { stationName } = req.body;
+        if (!stationId) {
+          return res.status(400).send("stationId is required");
+        }
+        const station = await db("se_project.stations")
+          .where("id", stationId)
+          .update({ stationName })
+          .returning("*");
+        return res.status(200).json(station);
+      }
+      else{
+        return res.status(400).send("You are Unauthorized to do this action");
+      }
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not update station");
+    }
+  });
+  
+  app.delete("/api/v1/station/:stationId", async function (req, res) {
+    try {
+      const user = await getUser(req);
+        if (user.isAdmin){
+        const { stationId } = req.params;
+        if (!stationId) {
+          return res.status(400).send("stationId is required");
+        }
+        const station = await db("se_project.stations")
+          .where("id", stationId)
+          .del()
+          .returning("*");
+        return res.status(200).json(station);
+        }
+        else
+          return res.status(400).send("You are Unauthorized to do this action")
+    }
+    catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not delete station");
+    }
+  });
+
+  app.put('/api/v1/requests/refunds/:requestId', async function (req, res){
+    try {
+      const user = await getUser(req);
+      if (user.isAdmin){
+        const { requestId } = req.params;
+        const { refundStaus } = req.body;
+        if (!requestId) {
+          return res.status(400).send("requestId is required");
+        }
+        if (!refundStaus) {
+          return res.status(400).send("status is required");
+        }
+        const request = await db("se_project.refund_requests")
+          .where("id", requestId)
+          .update({ status: refundStaus })
+          .returning("*");
+        return res.status(200).json(request);
+      }
+      else
+        return res.status(400).send("You are Unauthorized to do this action")
+    }
+    catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not accept/reject");
+    }
+  });
+
+  app.put('/api/v1/requests/senior/:requestId', async function (req, res){
+    try {
+      const user = await getUser(req);
+      if (user.isAdmin){
+        const { requestId } = req.params;
+        const { seniorStaus } = req.body;
+        if (!requestId) {
+          return res.status(400).send("requestId is required");
+        }
+        if (!seniorStaus) {
+          return res.status(400).send("status is required");
+        }
+        const request = await db("se_project.senior_requests")
+          .where("id", requestId)
+          .update({ status: seniorStaus })
+          .returning("*");
+        return res.status(200).json(request);
+      }
+      else
+        return res.status(400).send("You are Unauthorized to do this action")
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not accept/reject");
+    }
+  });
+
+  app.put('/api/v1/zones/:zoneId', async function (req, res){
+    try {
+      const user = await getUser(req);
+      if (user.isAdmin){
+        const { zoneId } = req.params;
+        const { price } = req.body;
+        if (!zoneId) {
+          return res.status(400).send("zoneId is required");
+        }
+        if (!price) {
+          return res.status(400).send("price is required");
+        }
+        const zone = await db("se_project.zones")
+          .where("id", zoneId)
+          .update({ price })
+          .returning("*");
+        return res.status(200).json(zone);
+      }
+      else
+        return res.status(400).send("You are Unauthorized to do this action")
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not update zone price");
+    }
+  });
+
 };
