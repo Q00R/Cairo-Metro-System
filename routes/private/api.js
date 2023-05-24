b3c92fc490a23c78d0b62a6fa3d8d591c7ca1717
Mostafa Ramadan <116387670+mostafaHossamEldin@users.noreply.github.com>
2023-05-24 22:04:27 +0300

    Last commit inshallah

--- routes/private/api.js
+++ routes/private/api.js
@@ -97,15 +97,85 @@ module.exports = function (app) {
     try {
       const user = await getUser(req);
         if (user.isAdmin){
-        const { stationId } = req.params;
-        if (!stationId) {
-          return res.status(400).send("stationId is required");
-        }
-        const station = await db("se_project.stations")
-          .where("id", stationId)
-          .del()
-          .returning("*");
-        return res.status(200).json(station);
+          const { stationId } = req.params;
+          if (!stationId) {
+            return res.status(400).send("stationId is required");
+          }
+          const station = await db("se_project.stations")
+            .where("id", stationId)
+            .returning("*");
+          const stationRoutes = await db("se_project.stationRoutes")
+            .innerJoin(
+              "se_project.routes",
+              "se_project.routes.id",
+              "se_project.stationRoutes.routeId"
+            )
+            .where("stationId", stationId)
+            .returning("se_project.routes.id", "se_project.routes.routeName", "se_project.routes.fromStationId", "se_project.routes.toStationId");
+          const routesIds = stationRoutes.map(route => route.routeId);
+          console.log("routesIds =>", routesIds);
+          let connectedStationsIds = (stationRoutes.map(route => (route.toStationId == stationId ? route.fromStationId : route.toStationId))).filter(id => id != stationId);
+          connectedStationsIds = connectedStationsIds.filter((id, index) => connectedStationsIds.indexOf(id) === index);
+          console.log("connectedStationsIds =>", connectedStationsIds);
+          let addedRoute;
+          for (let i = 0; i < connectedStationsIds.length-1; i++) {
+            for (let j = i+1; j < connectedStationsIds.length; j++) {
+              const newRoute1 = {
+                "fromStationId": connectedStationsIds[i],
+                "toStationId": connectedStationsIds[j],
+                "routeName": "New Route"
+              }
+              addedRoute = (await db("se_project.routes").insert(newRoute1).returning("*"));
+              console.log("addedRoute =>", addedRoute);
+              let id = addedRoute[0].id;
+              const newStationRoute1 = {
+                "stationId": newRoute1.fromStationId,
+                "routeId": id
+              }
+              console.log("newStationRoute1 =>", newStationRoute1);
+              const newStationRoute2 = {
+                "stationId": newRoute1.toStationId,
+                "routeId": id
+              }
+              console.log("newStationRoute2 =>", newStationRoute2);
+              await db("se_project.stationRoutes").insert(newStationRoute1).returning("*");
+              await db("se_project.stationRoutes").insert(newStationRoute2).returning("*");
+              const newRoute2 = {
+                "fromStationId": connectedStationsIds[j],
+                "toStationId": connectedStationsIds[i],
+                "routeName": "New Route"
+              }
+              addedRoute = (await db("se_project.routes").insert(newRoute2).returning("*"));
+              id = addedRoute[0].id;
+              console.log("addedRoute =>", addedRoute);
+              const newStationRoute3 = {
+                "stationId": newRoute2.fromStationId,
+                "routeId":  id
+              }
+              const newStationRoute4 = {
+                "stationId": newRoute2.toStationId,
+                "routeId":  id
+              }
+              await db("se_project.stationRoutes").insert(newStationRoute3).returning("*");
+              await db("se_project.stationRoutes").insert(newStationRoute4).returning("*");
+            }
+          }
+          const deletedStationRoutes1 = await db("se_project.stationRoutes")
+            .whereIn("routeId", routesIds)
+            .del()
+            .returning("*");
+          console.log("deletedStationRoutes1 =>", deletedStationRoutes1);
+          const deletedRoutes = await db("se_project.routes")
+            .whereIn("id", routesIds)
+            .del()
+            .returning("*");
+            console.log("deletedRoutes =>", deletedRoutes);
+          const deletedStation = await db("se_project.stations")
+            .where("id", stationId)
+            .del()
+            .returning("*");
+            console.log("deletedStation =>", deletedStation);
+          return res.status(200).json(deletedStation);
         }
         else
           return res.status(400).send("You are Unauthorized to do this action")
