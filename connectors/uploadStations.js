7ac9c7d6d6f44e37f2c85ec0b79df6f2071f8fe5
Ali Zein <alizein08042003@gmail.com>
2023-05-26 03:33:38 +0300

    Fixed refund ticket request and fixed some other minors

--- connectors/uploadStations.js
+++ connectors/uploadStations.js
@@ -1,6 +1,6 @@
 const db = require("./db");
-async function uploadSR() {
 
+async function uploadSR() {
   let SR = [
     { stationId: 1, routeId: 1 },
     { stationId: 1, routeId: 2 },
@@ -11,10 +11,10 @@ async function uploadSR() {
     { stationId: 3, routeId: 5 },
     { stationId: 3, routeId: 6 },
     { stationId: 3, routeId: 7 },
-    { stationId: 3, routeId: 8},
+    { stationId: 3, routeId: 8 },
     { stationId: 4, routeId: 5 },
     { stationId: 4, routeId: 6 },
-    { stationId: 4, routeId: 9},
+    { stationId: 4, routeId: 9 },
     { stationId: 4, routeId: 10 },
     { stationId: 5, routeId: 9 },
     { stationId: 5, routeId: 10 },
@@ -23,11 +23,13 @@ async function uploadSR() {
     { stationId: 6, routeId: 11 },
     { stationId: 6, routeId: 12 },
   ];
+
   for (let i = 0; i < SR.length; i++) {
-    const element =SR[i];
+    const element = SR[i];
     await db("se_project.stationRoutes").insert(element).returning("*");
   }
 }
+
 async function uploadS() {
   let stations = [
     {
@@ -75,31 +77,38 @@ async function uploadS() {
   ];
 
   for (let i = 0; i < stations.length; i++) {
-    const element =stations[i];
+    const element = stations[i];
     await db("se_project.stations").insert(element).returning("*");
   }
 }
+
 async function uploadR() {
-    let routes = [
-      { routeName: "hi12", fromStationId: 1, toStationId: 2 },
-      { routeName: "hi21", fromStationId: 2, toStationId: 1 },
-      { routeName: "hi23", fromStationId: 2, toStationId: 3 },
-      { routeName: "hi32", fromStationId: 3, toStationId: 2 },
-      { routeName: "hi34", fromStationId: 3, toStationId: 4 },
-      { routeName: "hi43", fromStationId: 4, toStationId: 3 },
-      { routeName: "hi36", fromStationId: 3, toStationId: 6 },
-      { routeName: "hi63", fromStationId: 6, toStationId: 3 },
-      { routeName: "hi45", fromStationId: 4, toStationId: 5 },
-      { routeName: "hi54", fromStationId: 5, toStationId: 4 },
-      { routeName: "hi76", fromStationId: 7, toStationId: 6 },
-      { routeName: "hi67", fromStationId: 6, toStationId: 7 },
-    ];
-  
+  let routes = [
+    { routeName: "hi12", fromStationId: 1, toStationId: 2 },
+    { routeName: "hi21", fromStationId: 2, toStationId: 1 },
+    { routeName: "hi23", fromStationId: 2, toStationId: 3 },
+    { routeName: "hi32", fromStationId: 3, toStationId: 2 },
+    { routeName: "hi34", fromStationId: 3, toStationId: 4 },
+    { routeName: "hi43", fromStationId: 4, toStationId: 3 },
+    { routeName: "hi36", fromStationId: 3, toStationId: 6 },
+    { routeName: "hi63", fromStationId: 6, toStationId: 3 },
+    { routeName: "hi45", fromStationId: 4, toStationId: 5 },
+    { routeName: "hi54", fromStationId: 5, toStationId: 4 },
+    { routeName: "hi76", fromStationId: 7, toStationId: 6 },
+    { routeName: "hi67", fromStationId: 6, toStationId: 7 },
+  ];
+
   for (let i = 0; i < routes.length; i++) {
-    const element =routes[i];
+    const element = routes[i];
     await db("se_project.routes").insert(element).returning("*");
   }
 }
-uploadS();
-uploadR();
-uploadSR();
+
+async function uploadData() {
+  await uploadS();
+  await uploadR();
+  await uploadSR();
+  process.exit(0);
+}
+
+uploadData();
--- routes/private/api.js
+++ routes/private/api.js
@@ -47,42 +47,98 @@ module.exports = function (app) {
       return res.status(400).send("Could not get users");
     }
   });
- 
+
+  // really bteshta8al bmazagha if we remove v1, it works perfectly fine
+  app.post("/api/v1/senior/request", async function(req, res){
+    let user = await getUser(req);
+    let userId = user.userId;
+    const {nationalId} = req.body;
+    const requestExists = await db("se_project.senior_requests").select("*").where("userId", userId).first();
+    if (isEmpty(requestExists)) 
+    {
+      const newRequest = 
+      {
+        status: "pending",
+        userId,
+        nationalId
+      }
+      try
+      {
+        const result = await db.insert(newRequest).into("se_project.senior_requests").returning("*");
+        return res.status(200).json(result); 
+      }
+      catch (e)
+      { 
+        console.log(e.message);
+        return res.status(400).send("Could not create senior request");
+      }
+    }
+    return res.status(400).send("This user already submitted a request to be a senior");
+  });
 
   
-  // doesn't want to work for some reason
-  
-
+// The calculate price is not working......
   app.post("/api/v1/refund/:ticketId", async function (req, res) {
-    const ticketId = req.params.ticketId;
+    const {ticketId} = req.params;
+    console.log("hena", ticketId);
+    const user = await getUser(req);
+    const userId = user.userId;
     const ticketQuery = await db
         .select("*")
         .from("se_project.tickets")
         .where("id", ticketId)
         .first();
-      if (isEmpty(ticketQuery)) {
-        return res.status(400).send("This ticket doesn't exist in the first place to be refunded");
-      }
-      else
+    if (isEmpty(ticketQuery)) {
+      return res.status(400).send("This ticket doesn't exist in the first place to be refunded");
+    }
+    if(ticketQuery.tripDate < (new Date()))
+    {
+      return res.status(400).send("This ticket is already expired");
+    }
+    if(ticketQuery.userId !== userId)
+    {
+      return res.status(400).send("This ticket doesn't belong to you");
+    }
+    const ticketPrice = await calculatePrice(ticketQuery.origin, ticketQuery.destination)
+    console.log("price", ticketPrice);
+    try
+    {
+      const newRefund =
       {
-        db.del().where('id', '==', ticketId)
+        status: "pending",
+        userId,
+        refundAmount: ticketPrice,
+        ticketId
       }
+      const refund = await db("se_project.refund_requests").insert(newRefund).returning("*");
+      return res.status(200).json(refund);
+      
+    }
+    catch (e)
+    {
+      console.log(e.message);
+      return res.status(400).send("Could not refund ticket");
+    }
+      
   });
 
 
 
-  // not yet tested as it requires some rides that are not yet created
+  // Works
   app.put("/api/v1/ride/simulate", async function(req, res) {
     const {origin, destination, tripDate } = req.body;
-    const userId = getUser(req).userId;
+    const user = await getUser(req);
+    const userId = user.userId;
+    console.log("user id", userId);
     try
     {
       const newRide = await db("se_project.rides")
       .where("origin", origin)
       .andWhere("destination", destination)
       .andWhere("tripDate", tripDate)
-      .andWhere(userId, userId)
-      .update("status", "completed").returning("*");
+      .andWhere("userId", userId)
+      .update("status", "completed")
+      .returning("*");
       return res.status(200).json(newRide);
     }
     catch (e) 
@@ -92,61 +148,6 @@ module.exports = function (app) {
     }
   });
 
-
-  // Bgad bteshta8al bmazagha
-  app.post("/api/v1/senior/request", async function (req, res) {
-    userId = await getUser(req).userId;
-    const requestExists = await db
-      .select("*")
-      .from("se_project.senior_requests")
-      .where("userId", userId);
-    if (!isEmpty(requestExists)) {
-      return res.status(400).send("This user already submitted a request to be a senior");
-    }
-    const newRequest = 
-    {
-      status: "pending",
-      userId,
-      nationalId: req.body.nationalId
-    }
-    try
-    {
-      const result = await db.insert(newRequest).into("se_project.senior_requests").returning("*");
-      return res.status(200).json(result); 
-    }
-    catch (e)
-    {
-      console.log(e.message);
-      return res.status(400).send("Could not create senior request");
-    }
-  });
-
-app.post("/api/v1/senior/request", async function (req, res) {
-  userId = await getUser(req).userId;
-  const requestExists = await db
-    .select("*")
-    .from("se_project.senior_requests")
-    .where("userId", userId);
-  if (!isEmpty(requestExists)) {
-    return res.status(400).send("This user already submitted a request to be a senior");
-  }
-  const newRequest = 
-  {
-    status: "pending",
-    userId,
-    nationalId: req.body.nationalId
-  }
-  try
-  {
-    const result = await db.insert(newRequest).into("se_project.senior_requests").returning("*");
-    return res.status(200).json(result); 
-  }
-  catch (e)
-  {
-    console.log(e.message);
-    return res.status(400).send("Could not create senior request");
-  }
-});
 
 //api to create a new route
 app.post("/api/v1/route",async function(req, res){
@@ -191,5 +192,170 @@ app.delete("/api/v1/route/:routeId",async function(req, res){
     return res.send(e.message);
   }
 })
-
 };
+// JavaScript code for printing shortest path between
+// two vertices of unweighted graph
+const max_value = 9007199254740992;
+
+// utility function to form edge between two vertices
+// source and dest
+function add_edge(adj, src, dest){
+ Number(src);
+  Number(dest);
+  console.log(typeof adj[src], typeof adj[src]);
+  
+	adj[src].push(dest);
+	adj[dest].push(src);
+  console.log(src, dest);
+}
+
+// a modified version of BFS that stores predecessor
+// of each vertex in array p
+// and its distance from source in array d
+function BFS(adj, src, dest, v, pred, dist)
+{
+  console.log("IN BFS");
+	// a queue to maintain queue of vertices whose
+	// adjacency list is to be scanned as per normal
+	// DFS algorithm
+	let queue = [];
+
+	// boolean array visited[] which stores the
+	// information whether ith vertex is reached
+	// at least once in the Breadth first search
+	let visited = new Array(v);
+
+	// initially all vertices are unvisited
+	// so v[i] for all i is false
+	// and as no path is yet constructed
+	// dist[i] for all i set to infinity
+	for (let i = 0; i < v; i++) {
+		visited[i] = false;
+		dist[i] = max_value;
+		pred[i] = -1;
+	}
+
+	// now source is first to be visited and
+	// distance from source to itself should be 0
+	visited[src] = true;
+	dist[src] = 0;
+	queue.push(src);
+
+	// standard BFS algorithm
+	while (queue.length > 0) {
+		let u = queue[0];
+		queue.shift();
+		for (let i = 0; i < adj[u].length; i++) {
+			if (visited[adj[u][i]] == false) {
+				visited[adj[u][i]] = true;
+				dist[adj[u][i]] = dist[u] + 1;
+				pred[adj[u][i]] = u;
+				queue.push(adj[u][i]);
+
+				// We stop BFS when we find
+				// destination.
+				if (adj[u][i] == dest){
+          console.log("vsited", visited);
+          return true;
+        }
+
+				
+			}
+		}
+    
+	}
+
+	return false;
+}
+
+// utility function to print the shortest distance
+// between source vertex and destination vertex
+function shortestDistance(adj, s, dest, v)
+{
+  console.log("IN SHORTEST DISTANCE");
+	// predecessor[i] array stores predecessor of
+	// i and distance array stores distance of i
+	// from s
+	let pred = new Array(v).fill(0);
+	let dist = new Array(v).fill(0);
+
+	if (BFS(adj, s, dest, v, pred, dist) == false) {
+		throw new Error("unreachable destination" );
+	}
+
+	// vector path stores the shortest path
+	let path = new Array();
+  
+	let crawl = dest;
+	path.push(crawl);
+	while (pred[crawl] != -1) {
+		path.push(pred[crawl]);
+  
+		crawl = pred[crawl];
+  }
+	// distance from source is in distance array
+  console.log("heloo");
+  console.log(dist[dest]);
+  console.log("Path is::");
+  let route="";
+	for (let i = path.length - 1; i >= 0; i--){
+		console.log(path[i]);
+    route+=path[i]
+  }
+  console.log("route", route,"concatenated",dist[dest]+route);
+    return dist[dest]+route;
+
+
+	// printing path from source to destination
+	
+}
+
+async function calculatePrice(source, dest){
+  // no. of vertices
+  console.log("IN CALCULATE PRICE");
+  let test = await db.count("*").from("se_project.stations");
+  console.log("test:", test[0].count);
+  let v = Number(test[0].count)+1;
+  console.log("printing V: ",v);
+
+  // array of vectors is used to store the graph
+  // in the form of an adjacency list
+  const adj = new Array(v).fill(0);
+
+  for(let i = 0; i < v; i++){
+    adj[i] = new Array();
+  }
+
+  // Creating graph given in the above diagram.
+  // add_edge function takes adjacency list, source
+  // and destination vertex as argument and forms
+  // an edge between them.
+  fromTo = await db.select("fromStationId","toStationId").from("se_project.routes");
+  
+  console.log(fromTo);
+  for(let i=0;i<fromTo.length;i++){
+    a=fromTo[i].fromStationId;
+    b=fromTo[i].toStationId;
+    
+    add_edge(adj,a,b);
+
+  }
+  console.log("HENAAAAAAAAAAAAAAA");
+  console.log(adj)
+ 
+  return shortestDistance(adj, source, dest, v);
+
+  // The code is contributed by Gautam goel
+}
+async function getStationName(stationId){
+  let entry =await db('se_project.stations').where('id', stationId).first();
+  let stationName=entry.stationName;
+  return stationName;
+ }
+
+ async function isStationTransfer(stationId){
+  let entry =await db('se_project.stations').where('id', stationId).first();
+  
+  let stationtype=entry.stationType;
+  return stationtype==="transfer";
+ }
