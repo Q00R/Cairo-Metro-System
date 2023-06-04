7c1de08ae02a151803345186f6b873f184d2e310
Ali Zein <alizein08042003@gmail.com>
2023-06-04 16:00:42 +0300

    Did some front end modifications

--- connectors/db.js
+++ connectors/db.js
@@ -10,8 +10,8 @@ const config = {
     host: "localhost",
     port: 5432,
     user: 'postgres',
-    password: 'Most123$',
-    database: 'postgres',
+    password: 'alizein123',
+    database: 'SE_MileStone2',
   }
 };
 
